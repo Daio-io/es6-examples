608272e3cd86936b01449c1c430ad358ddb1fa2e
Dai <dai.williams@bbc.co.uk>
2015-06-21 17:34:18 +0100

    added array.keys and aray.values examples

--- arrays/array.entries.js
+++ arrays/array.entries.js
@@ -14,3 +14,28 @@ console.log(arrIter.next().value); // [ 0, 'value1' ]
 console.log(arrIter.next().value); // [ 1, 'value2' ]
 console.log(arrIter.next().value); // [ 2, 'value3' ]
 console.log(arrIter.next().value); // undefined
+
+/*
+ Array.keys returns an array iterator object
+ which contains the key values of the array
+ */
+
+
+let arrIter2 = someArr.keys();
+
+console.log(arrIter2.next().value); // 0
+console.log(arrIter2.next().value); // 1
+console.log(arrIter2.next().value); // 2
+console.log(arrIter2.next().value); // undefined
+
+/*
+ Array.values returns an array iterator object
+ which contains the values of the array
+ */
+
+let arrIter3= someArr.values();
+
+console.log(arrIter3.next().value); // 0
+console.log(arrIter3.next().value); // 1
+console.log(arrIter3.next().value); // 2
+console.log(arrIter3.next().value); // undefined
